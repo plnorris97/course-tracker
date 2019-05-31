565abc23fb648a6cde56e672f9e55ee65d6872be
Tricia Norris <tricianorris97@gmail.com>
2019-05-31 13:53:34 -0400

    first commit with app

---
+++ course-tracker/src/app/courses-taken/courses-taken.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-taken',
+  templateUrl: './courses-taken.component.html',
+  styleUrls: ['./courses-taken.component.css']
+})
+export class CoursesTakenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
